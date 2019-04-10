cce6eff7c6bfcb27f891c34cde91af6bec415c75
Rose McClain <rosemcclain024@gmail.com>
2019-04-10 02:22:21 -0400

    cannot get the css to link or the jquery to actually work

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,12 +1,46 @@
 $(function(){
     $(".haventSeenYet").on("click", function(event) {
         events.preventDefault();
-
-
-        if (unseen == true) {
-            unseen == false;
+        var id = $(this).data("id");
+        var newDevour = $(this).data("haventSeenYet");
+            
+        var newDevouredState = {
+            devoured: newDevour
+        };
+            
+        if (newDevour == true) {
+            newDevour == false;
         } else {
-            unseen == true;
+            newDevour == true;
         }
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(function() {
+            console.log("changed stuff and it worked");
+            location.reload();
+        });
+    });
+
+
+    $(".create-form").on("submit", function(event) {
+    
+        event.preventDefault();
+
+        var newBurger = {
+        name: $("#ca").val().trim(),
+        newDevour: $("[name=newDevour]:").val().trim()
+        };
+
+    
+        $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+        }).then(
+        function() {
+            console.log("created new band");
+            location.reload();
+        });
     });
 });
